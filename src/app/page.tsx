bf0f2b6c40a1ad5ab38678833bd5e53bec2b61bb
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-10-11 22:15:36 +0000

    عايز الصوره بتاعت الهيرو سكشن  تاخد ويدس 100 في 100 و الهايت بتاعها يملا

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
   return (
     <div className="space-y-12">
-      <section className="relative h-screen w-full overflow-hidden bg-secondary shadow-lg -mx-4 -mt-8">
+      <section className="relative h-screen w-screen overflow-hidden bg-secondary shadow-lg -mx-4 -mt-8">
         {heroImage && (
           <Image
             src={heroImage.imageUrl}
