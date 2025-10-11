073c3af1bf1d71475ec6d64a2a7cc7586fe49acc
Adam Aber Desouky Hussien <a.hussien4503@su.edu.eg>
2025-10-11 21:35:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6944321321-ac5dc",
+  "appId": "1:692345076865:web:891fc0eafd62cb9e3da180",
+  "apiKey": "AIzaSyAM6-y8Emdb1q__pz92Rxn36aL5LFOzyvk",
+  "authDomain": "studio-6944321321-ac5dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "692345076865"
+};
